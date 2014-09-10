96449f1697e920835ef8cfa23da1af93e0544732
Andrwyw <kings_4@163.com>
2014-09-11 00:54:10 +0800

    modify UI

--- app/src/main/java/com/biunote/wyw/biunote/ui/adapter/NotesListAdapter.java
+++ app/src/main/java/com/biunote/wyw/biunote/ui/adapter/NotesListAdapter.java
@@ -1,6 +1,9 @@
 package com.biunote.wyw.biunote.ui.adapter;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +15,7 @@ import com.biunote.wyw.biunote.R;
 import com.biunote.wyw.biunote.model.NoteEntryModel;
 
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by wang on 2014/8/19.
@@ -76,8 +80,35 @@ public class NotesListAdapter extends BaseAdapter {
         NoteEntryModel model = mModelList.get(position);
         holder.tv_content.setText(model.getContent());
         holder.tv_crated_time.setText(model.getCreatedTime());
+        setRandomBg(holder);
 
         return convertView;
+    }
+
+    static int sLastIndex = 0;
+    @SuppressLint("NewApi")
+    private void setRandomBg(ViewHolder holder) {
+        int[] colorIntArray = mContext.getResources().getIntArray(R.array.grident_color_array);
+        Random random = new Random();
+        int index = 0;
+
+        do {
+            index = random.nextInt(colorIntArray.length);
+            //渐变颜色两两匹配,由奇->偶
+            if (index % 2 == 1){
+                index -= 1;
+            }
+        }while (sLastIndex == index);
+        sLastIndex = index;
+
+        int[] gradientColorArray = {colorIntArray[index],colorIntArray[index+1]};
+        GradientDrawable drawable = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT,gradientColorArray);
+        drawable.setCornerRadius(5);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN){
+            holder.rl_listview_row_container.setBackground(drawable);
+        }else{
+            holder.rl_listview_row_container.setBackgroundDrawable(drawable);
+        }
     }
 
     private final class ViewHolder {
